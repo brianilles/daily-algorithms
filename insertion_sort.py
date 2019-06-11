738c76c9638ec192ef03408b25ce3953d5d56186
Brian Illes <brianrilles@gmail.com>
2019-06-11 07:38:03 -0700

    Insertion sort complete

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,19 @@
-print("sanity")
+def insertion_sort(arr):
+    """
+    An efficient algorithm for sorting small arrays (here in python, lists).
+
+    Input: a list of numbers l
+    Output: list l sorted from smallest to largest
+    Runtime complexity: O(n^2)
+    """
+    for i in range(1, len(arr)):
+        num = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > num:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = num
+    return arr
+
+print(insertion_sort.__doc__)
+print(insertion_sort([10,9,6,4,1,2,11,25,3]))
