efb8cd8a6d92380ba54bfc86118fe205c78cf4cc
Brian Illes <brianrilles@gmail.com>
2019-06-13 07:37:47 -0700

    binary_search_recursive complete

--- binary_search_iterative.py
+++ binary_search_iterative.py
@@ -15,7 +15,7 @@ def binary_search_iterative(arr, target):
    
     low = 0
     high = len(arr) - 1
-    mid = (low + high) // 2
+    mid = (high - low) // 2
 
     if target > arr[-1]: 
         return False
--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -1,27 +1,34 @@
-def binary_search_recursive(arr, target):
+def binary_search_recursive(arr, target, low, high):
     """
     A recursive implementaion of binary search. Precondition: array must be sorted. Finding if a item exists in a list
     Input: Array, target value
     Output: Value's index or false
     Runtime Complexity: O(log(n))
     """
-    low = 0
-    high = (len(arr) - 1)
-    midpoint = (high + low) // 2
+    middle = (low + high) // 2
 
+    if len(arr) == 0:
+        return False
+    
     if target > arr[-1]: 
         return False
     if target < arr[0]:
         return False
 
-    if target == arr[midpoint]:
-        return midpoint
-    elif target < arr[midpoint]:
-        return binary_search_recursive(arr[:midpoint], target)
-    elif target > arr[midpoint]:
-        return binary_search_recursive(arr[midpoint:], target)
+    if arr[middle] == target:
+        return middle
+    elif target < arr[middle]:
+        high = middle
+        return binary_search_recursive(arr, target, low, high)
+    elif target > arr[middle]:
+        low = middle
+        return binary_search_recursive(arr, target, low, high)
+    else:
+        return False
+
 
 print(binary_search_recursive.__doc__)
-print(binary_search_recursive([1,2,3,4,5,6,7,8,9], 5))
-print(binary_search_recursive([1,2,3,4,5,6,7,8,9], 6))
-print(binary_search_recursive([1,2,3,4,5,6,7,8,9], -10))
+arr1 = [1,2,3,4,5,6,7,8,9,10]
+print(binary_search_recursive(arr1, 4, 0, len(arr1) - 1))
+print(binary_search_recursive(arr1, 9, 0, len(arr1) - 1))
+print(binary_search_recursive(arr1, 12, 0, len(arr1) - 1))
