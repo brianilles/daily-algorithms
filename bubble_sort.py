7ba3510cb1302b2a8f94f8e0c28c5b3adb51bb54
Brian Illes <brianrilles@gmail.com>
2019-06-17 08:10:31 -0700

    Completes bubble_sort_recursive.py

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    """
+    Input: an array (here in Python, a list)
+    Output: the array sorted
+    Runtime Complexity: 
+    """
+    for i in range(0, len(arr) - 1):
+        swapped = False
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            swapped = True
+            bubble_sort(arr)
+    return arr
+
+print(bubble_sort.__doc__)
+print(bubble_sort([2,52,42, 123, 9, 6, 3,2,1]))
